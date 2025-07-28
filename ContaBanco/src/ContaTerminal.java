f3eb8398d7be59d570a7e100d82f5792b49abbf8
feepereira <feepereira17@gmail.com>
2025-07-28 19:26:58 -0300

    Conta Terminal - Desafio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
 
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o seu nome: ");
+        nomeCliente = scanner.nextLine ();
+        
+        System.out.println("Por favor, digite o número da agência: ");
+        agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da conta: ");
+        numero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o seu saldo atual: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("\"Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque.");
         
     }
 }
